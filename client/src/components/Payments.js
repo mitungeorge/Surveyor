b9f138f1f5c9402da6a4ea7f7f861a93e59a8193
Mitun George <mitungeorge@MITUNs-MacBook-Pro.local>
2018-02-10 19:17:47 -0800

    Added billing and client side app

---
+++ client/src/components/Payments.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Surveyor"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn blue-grey darken-1">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default connect(null, actions)(Payments);
